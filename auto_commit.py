bcc814c340f218d702c77e9d1c7c64112b87991e
Chongjian Tang <chongjian.tang@gridraster.com>
2025-04-17 16:02:06 -0700

    Update.

--- auto_commit.py
+++ auto_commit.py
@@ -161,7 +161,7 @@ def git_commit(repo_path, file_path, message):
         if file_path:
             command_args.append(file_path)
 
-        command_args.append(["-m", message])
+        command_args += ["-m", message]
 
         subprocess.run(
             command_args,
